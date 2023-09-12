c2749ed3c2e7c596947410c0d0f9e3c20f4e6745
Josue Hernandez <joseh1337@gmail.com>
2023-09-12 00:02:53 -0700

    added save button for localstorage and made a box appear to notify that it's been saved

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,46 @@ $(function () {
 
 var currentHour = dayjs().hour();
 
+$(".time-block").each(function () {
+  var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+  if (blockHour < currentHour) {
+    $(this).addClass("past");
+  } else if (blockHour === currentHour) {
+    $(this).addClass("present");
+  } else {
+    $(this).addClass("future");
+  }
+});
+
+$(".saveBtn").on("click", function () {
+  var blockId = $(this).closest(".time-block").attr("id");
+  var userEvent = $(this).siblings(".description").val();
+
+  localStorage.setItem(blockId, userEvent);
+
+  var notification = $("<div>")
+    .addClass("notification")
+    .text("Appointment Added to localStorage")
+    .insertBefore($(this).closest(".row"));
+
+  notification.slideDown();
+
+  setTimeout(function () {
+    notification.slideUp(function () {
+      $(this).remove();
+    });
+  }, 1500);
+});
+
+
+$(function () {
+  $(".time-block").each(function () {
+    var blockId = $(this).attr("id");
+    var savedEvent = localStorage.getItem(blockId);
+
+    if (savedEvent) {
+      $(this).find(".description").val(savedEvent);
+    }
+  });
+});
